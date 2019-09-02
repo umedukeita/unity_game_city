f2a17fdf4bf49b75d1e35240b1f1645d61cedcae
umedukeita <kitumd2@gmail.com>
2019-09-02 16:26:09 +0900

    Add Project

---
+++ The Tower/Assets/User/Script/ImageBoxCon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageBoxCon : MonoBehaviour {
+
+    public MeshFilter Image;
+    public PlayerCon player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.select >= 0)
+        {
+            Image.sharedMesh = player.itemtype[player.objNumber[player.select]].GetComponent<MeshFilter>().sharedMesh;
+        }
+    }
+}
---
+++ The Tower/Assets/User/Script/Item.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Item : MonoBehaviour {
+
+    public PlayerCon player;
+    
+
+    private Animator animator;
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        //Select();
+        //Image();
+
+
+       
+    }
+
+    
+    
+
+}
